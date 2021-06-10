f594e7a2122f1f74af5a78654f8fb03c97254173
bes-germes <olezhka.kruzhkov@mail.ru>
2021-06-10 16:20:04 +0300

    commit

--- src/main/java/com/example/scannerapi/check/CheckController.java
+++ src/main/java/com/example/scannerapi/check/CheckController.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,38 +42,50 @@ public class CheckController {
 //        }
 
 
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        String jsonString = null;
+
+        try {
+            System.out.println(clipboard.getData(DataFlavor.stringFlavor));
+            System.out.println(clipboard.getData(DataFlavor.stringFlavor).getClass().getSimpleName());
+            jsonString = (String) clipboard.getData(DataFlavor.stringFlavor);
+        } catch (UnsupportedFlavorException | IOException e) {
+            e.printStackTrace();
+        }
+
         CheckModel checkModel = new CheckModel();
-        return "[\n" +
-                "    {\n" +
-                "        \"sum\": 300,\n" +
-                "        \"name\": \"Пиво\",\n" +
-                "        \"quantity\": 2,\n" +
-                "        \"price\": 150\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 300,\n" +
-                "        \"name\": \"Водка\",\n" +
-                "        \"quantity\": 1,\n" +
-                "        \"price\": 300\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 520,\n" +
-                "        \"name\": \"Вино\",\n" +
-                "        \"quantity\": 2,\n" +
-                "        \"price\": 260\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 72,\n" +
-                "        \"name\": \"Кириешки\",\n" +
-                "        \"quantity\": 3,\n" +
-                "        \"price\": 24\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 241,\n" +
-                "        \"name\": \"Сыр КИПР. рос.\",\n" +
-                "        \"quantity\": 1,\n" +
-                "        \"price\": 241\n" +
-                "    }\n" +
-                "]";
+        return jsonString;
+                //"[\n" +
+//                "    {\n" +
+//                "        \"sum\": 300,\n" +
+//                "        \"name\": \"Пиво\",\n" +
+//                "        \"quantity\": 2,\n" +
+//                "        \"price\": 150\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "        \"sum\": 300,\n" +
+//                "        \"name\": \"Водка\",\n" +
+//                "        \"quantity\": 1,\n" +
+//                "        \"price\": 300\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "        \"sum\": 520,\n" +
+//                "        \"name\": \"Вино\",\n" +
+//                "        \"quantity\": 2,\n" +
+//                "        \"price\": 260\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "        \"sum\": 72,\n" +
+//                "        \"name\": \"Кириешки\",\n" +
+//                "        \"quantity\": 3,\n" +
+//                "        \"price\": 24\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "        \"sum\": 241,\n" +
+//                "        \"name\": \"Сыр КИПР. рос.\",\n" +
+//                "        \"quantity\": 1,\n" +
+//                "        \"price\": 241\n" +
+//                "    }\n" +
+//                "]";
     }
 }
