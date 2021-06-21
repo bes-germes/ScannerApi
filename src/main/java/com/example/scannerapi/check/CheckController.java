1c8682fbcad19c85d4ef3b50584b397038bf8766
bes-germes <olezhka.kruzhkov@mail.ru>
2021-06-21 22:43:27 +0300

    MbFinal2

--- src/main/java/com/example/scannerapi/check/CheckController.java
+++ src/main/java/com/example/scannerapi/check/CheckController.java
@@ -127,51 +127,51 @@ public class CheckController {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         String jsonString = null;
 
-        try {
-            System.out.println(clipboard.getData(DataFlavor.stringFlavor));
-            System.out.println(clipboard.getData(DataFlavor.stringFlavor).getClass().getSimpleName());
-            jsonString = (String) clipboard.getData(DataFlavor.stringFlavor);
-            return jsonString;
-        } catch (UnsupportedFlavorException | IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            System.out.println(clipboard.getData(DataFlavor.stringFlavor));
+//            System.out.println(clipboard.getData(DataFlavor.stringFlavor).getClass().getSimpleName());
+//            jsonString = (String) clipboard.getData(DataFlavor.stringFlavor);
+//            return jsonString;
+//        } catch (UnsupportedFlavorException | IOException e) {
+//            e.printStackTrace();
+//        }
 
         System.out.println("Hello, logs!??");
         LOGGER.log(Level.INFO,"STEPA plz ");
 //        CheckModel checkModel = new CheckModel();
 //        return jsonString;
-          return
-          "[\n" +
-                "    {\n" +
-                "        \"sum\": 300,\n" +
-                "        \"name\": \"Пиво\",\n" +
-                "        \"quantity\": 2,\n" +
-                "        \"price\": 150\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 300,\n" +
-                "        \"name\": \"Водка\",\n" +
-                "        \"quantity\": 1,\n" +
-                "        \"price\": 300\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 520,\n" +
-                "        \"name\": \"Вино\",\n" +
-                "        \"quantity\": 2,\n" +
-                "        \"price\": 260\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 72,\n" +
-                "        \"name\": \"Кириешки\",\n" +
-                "        \"quantity\": 3,\n" +
-                "        \"price\": 24\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 241,\n" +
-                "        \"name\": \"Сыр КИПР. рос.\",\n" +
-                "        \"quantity\": 1,\n" +
-                "        \"price\": 241\n" +
-                "    }\n" +
-                "]";
+          return "STRIN?";
+//          "[\n" +
+//                "    {\n" +
+//                "        \"sum\": 300,\n" +
+//                "        \"name\": \"Пиво\",\n" +
+//                "        \"quantity\": 2,\n" +
+//                "        \"price\": 150\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "        \"sum\": 300,\n" +
+//                "        \"name\": \"Водка\",\n" +
+//                "        \"quantity\": 1,\n" +
+//                "        \"price\": 300\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "        \"sum\": 520,\n" +
+//                "        \"name\": \"Вино\",\n" +
+//                "        \"quantity\": 2,\n" +
+//                "        \"price\": 260\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "        \"sum\": 72,\n" +
+//                "        \"name\": \"Кириешки\",\n" +
+//                "        \"quantity\": 3,\n" +
+//                "        \"price\": 24\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "        \"sum\": 241,\n" +
+//                "        \"name\": \"Сыр КИПР. рос.\",\n" +
+//                "        \"quantity\": 1,\n" +
+//                "        \"price\": 241\n" +
+//                "    }\n" +
+//                "]";
     }
 }
