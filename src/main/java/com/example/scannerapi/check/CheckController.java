3983e0a4fdfab9c470b538c353f0d207556d0f41
bes-germes <olezhka.kruzhkov@mail.ru>
2021-06-21 22:56:17 +0300

    MbFinal4

--- src/main/java/com/example/scannerapi/check/CheckController.java
+++ src/main/java/com/example/scannerapi/check/CheckController.java
@@ -171,38 +171,38 @@ public class CheckController {
         LOGGER.log(Level.INFO,"STEPA plz ");
 //        CheckModel checkModel = new CheckModel();
 //        return jsonString;
-          return "111";
-//          "[\n" +
-//                "    {\n" +
-//                "        \"sum\": 300,\n" +
-//                "        \"name\": \"Пиво\",\n" +
-//                "        \"quantity\": 2,\n" +
-//                "        \"price\": 150\n" +
-//                "    },\n" +
-//                "    {\n" +
-//                "        \"sum\": 300,\n" +
-//                "        \"name\": \"Водка\",\n" +
-//                "        \"quantity\": 1,\n" +
-//                "        \"price\": 300\n" +
-//                "    },\n" +
-//                "    {\n" +
-//                "        \"sum\": 520,\n" +
-//                "        \"name\": \"Вино\",\n" +
-//                "        \"quantity\": 2,\n" +
-//                "        \"price\": 260\n" +
-//                "    },\n" +
-//                "    {\n" +
-//                "        \"sum\": 72,\n" +
-//                "        \"name\": \"Кириешки\",\n" +
-//                "        \"quantity\": 3,\n" +
-//                "        \"price\": 24\n" +
-//                "    },\n" +
-//                "    {\n" +
-//                "        \"sum\": 241,\n" +
-//                "        \"name\": \"Сыр КИПР. рос.\",\n" +
-//                "        \"quantity\": 1,\n" +
-//                "        \"price\": 241\n" +
-//                "    }\n" +
-//                "]";
+          return
+          "[\n" +
+                "    {\n" +
+                "        \"sum\": 300,\n" +
+                "        \"name\": \"Пиво\",\n" +
+                "        \"quantity\": 2,\n" +
+                "        \"price\": 150\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"sum\": 300,\n" +
+                "        \"name\": \"Водка\",\n" +
+                "        \"quantity\": 1,\n" +
+                "        \"price\": 300\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"sum\": 520,\n" +
+                "        \"name\": \"Вино\",\n" +
+                "        \"quantity\": 2,\n" +
+                "        \"price\": 260\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"sum\": 72,\n" +
+                "        \"name\": \"Кириешки\",\n" +
+                "        \"quantity\": 3,\n" +
+                "        \"price\": 24\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"sum\": 241,\n" +
+                "        \"name\": \"Сыр КИПР. рос.\",\n" +
+                "        \"quantity\": 1,\n" +
+                "        \"price\": 241\n" +
+                "    }\n" +
+                "]";
     }
 }
