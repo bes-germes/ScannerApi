fd43daaa366d10b48a427b1a6b132334ceb746da
bes-germes <olezhka.kruzhkov@mail.ru>
2021-06-21 21:55:15 +0300

    test log

--- src/main/java/com/example/scannerapi/check/CheckController.java
+++ src/main/java/com/example/scannerapi/check/CheckController.java
@@ -31,8 +31,7 @@ public class CheckController {
 //        options.addArguments("--disable-gpu");
 //        options.addArguments("--no-sandbox");
 //        options.setBinary("C:\\bin\\chromedriver.exe");
-//        ChromeDriver driver = new ChromeDriver(options);
-//        driver.get("");
+
 //        char[] information = info.toCharArray();
 //
 //        List<String> parsedInfo = new ArrayList<>();
@@ -52,79 +51,88 @@ public class CheckController {
 //            tmp.add(c);
 //        }
 
-//        WebDriver driver = new ChromeDriver();
-//        JavascriptExecutor executor = (JavascriptExecutor)driver;
-//        driver.get("https://proverkacheka.com/");
-//
-//        WebDriverWait wait1 = new WebDriverWait(driver, 10);
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='.b-checkform_tab-props']"))).click();
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fn"))).click();
-//        WebElement chekform_fn = driver.findElement(By.id("b-checkform_fn"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fn);
-//        chekform_fn.sendKeys("9285440300237214");
-//
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fd"))).click();
-//        WebElement chekform_fd = driver.findElement(By.id("b-checkform_fd"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fd);
-//        chekform_fd.sendKeys("19789");
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fp"))).click();
-//        WebElement chekform_fp = driver.findElement(By.id("b-checkform_fp"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fp);
-//        chekform_fp.sendKeys("0643502697");
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_s form-control']"))).click();
-//        WebElement sum = driver.findElement(By.xpath("//input[@class='b-checkform_s form-control']"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", sum);
-//        sum.sendKeys("779,36");
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_date form-control']"))).click();
-//        WebElement date = driver.findElement(By.xpath("//input[@class='b-checkform_date form-control']"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", date);
-//        date.sendKeys("18052021");
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_time form-control']"))).click();
-//        WebElement time = driver.findElement(By.xpath("//input[@class='b-checkform_time form-control']"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", time);
-//        time.sendKeys("1949");
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@id='b-checkform_n']"))).click();
-//        WebElement checkform_n = driver.findElement(By.xpath("//select[@id='b-checkform_n']"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", checkform_n);
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[@value='1']"))).click();
-//        WebElement value_1 = driver.findElement(By.xpath("//option[@value='1']"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", value_1);
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='b-checkform_btn-send btn btn-primary btn-sm pull-right']"))).click();
-//        WebElement value_2 = driver.findElement(By.xpath("//button[@class='b-checkform_btn-send btn btn-primary btn-sm pull-right']"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", value_2);
-//
-//        executor.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='btn btn-primary btn-sm dropdown-toggle']"))).click();
-//        WebElement fin = driver.findElement(By.xpath("//button[@class='btn btn-primary btn-sm dropdown-toggle']"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", fin);
-//
-//        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='b-check_btn-buffer']"))).click();
-//        WebElement buffer = driver.findElement(By.xpath("//a[@class='b-check_btn-buffer']"));
-//        executor.executeScript("arguments[0].scrollIntoView(true);", buffer);
-//
-//        driver.quit();
-//
-//        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-//        String jsonString = null;
-//
-//        try {
-//            System.out.println(clipboard.getData(DataFlavor.stringFlavor));
-//            System.out.println(clipboard.getData(DataFlavor.stringFlavor).getClass().getSimpleName());
-//            jsonString = (String) clipboard.getData(DataFlavor.stringFlavor);
-//        } catch (UnsupportedFlavorException | IOException e) {
-//            e.printStackTrace();
-//        }
-        System.out.println("Hello, logs!");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.setBinary("GOOGLE_CHROME_BIN");
+
+        WebDriver driver = new ChromeDriver(options);
+        System.out.println(driver.getPageSource());
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        driver.get("https://proverkacheka.com/");
+
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='.b-checkform_tab-props']"))).click();
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fn"))).click();
+        WebElement chekform_fn = driver.findElement(By.id("b-checkform_fn"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fn);
+        chekform_fn.sendKeys("9285440300237214");
+
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fd"))).click();
+        WebElement chekform_fd = driver.findElement(By.id("b-checkform_fd"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fd);
+        chekform_fd.sendKeys("19789");
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fp"))).click();
+        WebElement chekform_fp = driver.findElement(By.id("b-checkform_fp"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fp);
+        chekform_fp.sendKeys("0643502697");
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_s form-control']"))).click();
+        WebElement sum = driver.findElement(By.xpath("//input[@class='b-checkform_s form-control']"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", sum);
+        sum.sendKeys("779,36");
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_date form-control']"))).click();
+        WebElement date = driver.findElement(By.xpath("//input[@class='b-checkform_date form-control']"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", date);
+        date.sendKeys("18052021");
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_time form-control']"))).click();
+        WebElement time = driver.findElement(By.xpath("//input[@class='b-checkform_time form-control']"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", time);
+        time.sendKeys("1949");
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@id='b-checkform_n']"))).click();
+        WebElement checkform_n = driver.findElement(By.xpath("//select[@id='b-checkform_n']"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", checkform_n);
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[@value='1']"))).click();
+        WebElement value_1 = driver.findElement(By.xpath("//option[@value='1']"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", value_1);
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='b-checkform_btn-send btn btn-primary btn-sm pull-right']"))).click();
+        WebElement value_2 = driver.findElement(By.xpath("//button[@class='b-checkform_btn-send btn btn-primary btn-sm pull-right']"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", value_2);
+
+        executor.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='btn btn-primary btn-sm dropdown-toggle']"))).click();
+        WebElement fin = driver.findElement(By.xpath("//button[@class='btn btn-primary btn-sm dropdown-toggle']"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", fin);
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='b-check_btn-buffer']"))).click();
+        WebElement buffer = driver.findElement(By.xpath("//a[@class='b-check_btn-buffer']"));
+        executor.executeScript("arguments[0].scrollIntoView(true);", buffer);
+
+        driver.quit();
+
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        String jsonString = null;
+
+
+        try {
+            System.out.println(clipboard.getData(DataFlavor.stringFlavor));
+            System.out.println(clipboard.getData(DataFlavor.stringFlavor).getClass().getSimpleName());
+            jsonString = (String) clipboard.getData(DataFlavor.stringFlavor);
+        } catch (UnsupportedFlavorException | IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Hello, logs!??");
+
 //        CheckModel checkModel = new CheckModel();
 //        return jsonString;
           return"[\n" +
