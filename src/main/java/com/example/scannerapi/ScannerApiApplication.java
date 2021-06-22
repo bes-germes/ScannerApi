800085b50c47c7bb8b952782d14de22b1cc12f03
bes-germes <olezhka.kruzhkov@mail.ru>
2021-06-22 17:15:05 +0300

    Final

--- src/main/java/com/example/scannerapi/ScannerApiApplication.java
+++ src/main/java/com/example/scannerapi/ScannerApiApplication.java
@@ -2,12 +2,19 @@ package com.example.scannerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ScannerApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScannerApiApplication.class, args);
     }
 
+    @GetMapping
+    public String hello(){
+        return "Server is working";
+    }
 }
--- src/main/java/com/example/scannerapi/check/CheckController.java
+++ src/main/java/com/example/scannerapi/check/CheckController.java
@@ -1,4 +1,5 @@
 package com.example.scannerapi.check;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -29,42 +30,25 @@ public class CheckController {
     static Logger LOGGER;
 
     @GetMapping()
-    public String getCheck() {
+    public String getCheck(@RequestParam String checkString) {
 
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-gpu");
-//        options.addArguments("--no-sandbox");
-//        options.setBinary("C:\\bin\\chromedriver.exe");
+        String[] elementsArray = checkString.split("&");
 
-//        char[] information = info.toCharArray();
-//
-//        List<String> parsedInfo = new ArrayList<>();
-//        List<Character> tmp = new ArrayList<>();
-//        for (char c : information) {
-//            if (c == '&') {
-//                Character firstCharacter = tmp.get(0);
-//                if (firstCharacter.equals('t')
-//                        || firstCharacter.equals('s')
-//                        || firstCharacter.equals('i')) {
-//                    parsedInfo.add(tmp.toString().substring(2));
-//                } else if (firstCharacter.equals('f')) {
-//                    parsedInfo.add(tmp.toString().substring(3));
-//                }
-//                tmp.clear();
-//            }
-//            tmp.add(c);
-//        }
-
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.setBinary("GOOGLE_CHROME_BIN");
+        for (int i = 0; i < elementsArray.length; ++i) {
+            if (elementsArray[i].startsWith("f")) {
+                elementsArray[i] = elementsArray[i].substring(3);
+            } else {
+                elementsArray[i] = elementsArray[i].substring(2);
+            }
+        }
+        System.out.println("");
+        String beforeT = elementsArray[0].substring(0, elementsArray[0].lastIndexOf("T"));
+        String afterT = elementsArray[0].substring(elementsArray[0].lastIndexOf("T") + 1);
 
         WebDriver driver = new ChromeDriver();
-        System.out.println(driver.getPageSource());
-        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
         driver.get("https://proverkacheka.com/");
+
 
         WebDriverWait wait1 = new WebDriverWait(driver, 10);
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='.b-checkform_tab-props']"))).click();
@@ -72,33 +56,33 @@ public class CheckController {
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fn"))).click();
         WebElement chekform_fn = driver.findElement(By.id("b-checkform_fn"));
         executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fn);
-        chekform_fn.sendKeys("9285440300237214");
+        chekform_fn.sendKeys(elementsArray[2]);
 
 
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fd"))).click();
         WebElement chekform_fd = driver.findElement(By.id("b-checkform_fd"));
         executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fd);
-        chekform_fd.sendKeys("19789");
+        chekform_fd.sendKeys(elementsArray[3]);
 
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("b-checkform_fp"))).click();
         WebElement chekform_fp = driver.findElement(By.id("b-checkform_fp"));
         executor.executeScript("arguments[0].scrollIntoView(true);", chekform_fp);
-        chekform_fp.sendKeys("0643502697");
+        chekform_fp.sendKeys(elementsArray[4]);
 
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_s form-control']"))).click();
         WebElement sum = driver.findElement(By.xpath("//input[@class='b-checkform_s form-control']"));
         executor.executeScript("arguments[0].scrollIntoView(true);", sum);
-        sum.sendKeys("779,36");
+        sum.sendKeys(elementsArray[1]);
 
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_date form-control']"))).click();
         WebElement date = driver.findElement(By.xpath("//input[@class='b-checkform_date form-control']"));
         executor.executeScript("arguments[0].scrollIntoView(true);", date);
-        date.sendKeys("18052021");
+        date.sendKeys(beforeT.substring(6) + beforeT.substring(4, 6) + beforeT.substring(0, 4));
 
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='b-checkform_time form-control']"))).click();
         WebElement time = driver.findElement(By.xpath("//input[@class='b-checkform_time form-control']"));
         executor.executeScript("arguments[0].scrollIntoView(true);", time);
-        time.sendKeys("1949");
+        time.sendKeys(afterT.substring(0, 4));
 
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@id='b-checkform_n']"))).click();
         WebElement checkform_n = driver.findElement(By.xpath("//select[@id='b-checkform_n']"));
@@ -123,86 +107,18 @@ public class CheckController {
         executor.executeScript("arguments[0].scrollIntoView(true);", buffer);
 
         driver.quit();
-//
-//        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-//        String jsonString = null;
-//
-//        try {
-//            System.out.println(clipboard.getData(DataFlavor.stringFlavor));
-//            System.out.println(clipboard.getData(DataFlavor.stringFlavor).getClass().getSimpleName());
-//            jsonString = (String) clipboard.getData(DataFlavor.stringFlavor);
-//            return           "[\n" +
-//                    "    {\n" +
-//                    "        \"sum\": 300,\n" +
-//                    "        \"name\": \"Пиво\",\n" +
-//                    "        \"quantity\": 2,\n" +
-//                    "        \"price\": 150\n" +
-//                    "    },\n" +
-//                    "    {\n" +
-//                    "        \"sum\": 300,\n" +
-//                    "        \"name\": \"Водка\",\n" +
-//                    "        \"quantity\": 1,\n" +
-//                    "        \"price\": 300\n" +
-//                    "    },\n" +
-//                    "    {\n" +
-//                    "        \"sum\": 520,\n" +
-//                    "        \"name\": \"Вино\",\n" +
-//                    "        \"quantity\": 2,\n" +
-//                    "        \"price\": 260\n" +
-//                    "    },\n" +
-//                    "    {\n" +
-//                    "        \"sum\": 72,\n" +
-//                    "        \"name\": \"Кириешки\",\n" +
-//                    "        \"quantity\": 3,\n" +
-//                    "        \"price\": 24\n" +
-//                    "    },\n" +
-//                    "    {\n" +
-//                    "        \"sum\": 241,\n" +
-//                    "        \"name\": \"Сыр КИПР. рос.\",\n" +
-//                    "        \"quantity\": 1,\n" +
-//                    "        \"price\": 241\n" +
-//                    "    }\n" +
-//                    "]";
-//        } catch (UnsupportedFlavorException | IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println("Hello, logs!??");
-//        LOGGER.log(Level.INFO,"STEPA plz ");
-//        CheckModel checkModel = new CheckModel();
-//        return jsonString;
-          return
-          "[\n" +
-                "    {\n" +
-                "        \"sum\": 300,\n" +
-                "        \"name\": \"Пиво\",\n" +
-                "        \"quantity\": 2,\n" +
-                "        \"price\": 150\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 300,\n" +
-                "        \"name\": \"Водка\",\n" +
-                "        \"quantity\": 1,\n" +
-                "        \"price\": 300\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 520,\n" +
-                "        \"name\": \"Вино\",\n" +
-                "        \"quantity\": 2,\n" +
-                "        \"price\": 260\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 72,\n" +
-                "        \"name\": \"Кириешки\",\n" +
-                "        \"quantity\": 3,\n" +
-                "        \"price\": 24\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"sum\": 241,\n" +
-                "        \"name\": \"Сыр КИПР. рос.\",\n" +
-                "        \"quantity\": 1,\n" +
-                "        \"price\": 241\n" +
-                "    }\n" +
-                "]";
+
+        System.setProperty("java.awt.headless", "false");
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Clipboard clipboard = tk.getSystemClipboard();
+
+        try {
+            String result = clipboard.getData(DataFlavor.stringFlavor).toString();
+            return result.substring(result.lastIndexOf("["), result.lastIndexOf("]") + 1);
+        } catch (UnsupportedFlavorException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
